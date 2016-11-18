bb1a0df68304c361fb11680f67a3ef352bc160cf
TheilCamille <camille.theil@gmail.com>
2016-11-18 10:20:03 +0100

    18/11/2016

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+//exo 1
+
+for($i=0; $i< 10; $i++)
+	{
+		echo $i;
+	}
+	
+//exo 2
+
+$b = 77;
+for($a=0; $a<20; $a++)
+	{
+		$c = $a*$b;
+		echo "</br>".$c;
+	}
+
+//exo 3
+
+$bb = 77;
+for($aa=100; $aa >= 10; $aa--)
+	{
+		$cc = $aa*$bb;
+		echo "</br>".$cc;
+	}
+
+//exo 4
+
+for($i=1; $i<10; $i*=1.5)
+	{
+		echo "</br>".$i;
+	}
+
+//exo 5
+
+for($i=1; $i<15; $i++)
+	{
+		echo "</br>On y arrive presque.";
+	}
+
+//exo 6
+
+for($i=20; $i>0; $i--)
+	{
+		echo "</br>C'est presque bon.";
+	}
+
+//exo 7
+
+for($i=1; $i<100; $i+=15)
+	{
+		echo "</br>On tient le bon bout.";
+	}
+
+//exo 8
+
+for($i=200; $i>0; $i-=12)
+	{
+		echo "</br>Enfin !!!!";
+	}	
+
+
+?>
